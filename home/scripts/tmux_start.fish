3350ce27188908e1164b9b4e6b81e1d4390d5943
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-17 19:21:16 +0100

    wip

--- home/scripts/tmux_start.fish
+++ home/scripts/tmux_start.fish
@@ -1,3 +1,11 @@
 #!/usr/bin/env fish
 
+#nix-build -E 'let pkgs = import <nixpkgs> {}; tokyonight = pkgs.tmuxPlugins.mkTmuxPlugin { pluginName = "tokyonight"; version = "latest"; src = pkgs.fetchFromGitHub { owner = "fabioluciano"; repo = "tmux-tokyo-night"; rev = "main"; sha256 = "sha256-9nDgiJptXIP+Hn9UY+QFMgoghw4HfTJ5TZq0f9KVOFg="; }; }; in tokyonight'
+#
+#/nix/store/674ar0bcq6ilkc4iyipwb6nh0qy613m0-tmuxplugin-tokyonight-latest
+
+#nix eval nixpkgs#tmuxPlugins.gruvbox.outPath 
+#
+#"/nix/store/idpp3gwvq2bh1ak6jbap1q9n064122an-tmuxplugin-gruvbox-unstable-2022-04-19"
+
 tmux attach-session
