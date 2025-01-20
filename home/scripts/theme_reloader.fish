e81e05cfe6e76daf52748d483e2d4fb093ea2b22
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-20 11:15:36 +0100

    WIP

--- home/scripts/theme_reloader.fish
+++ home/scripts/theme_reloader.fish
@@ -13,22 +13,30 @@ function hyprland_reload
     hyprctl reload
 end
 
+echo $argv[1]
 if test "$argv[1]" = "--tokyonight-dark"
-    set -Ux THEME "tokyonight-dark"
+    echo "tokyonight-dark choice"
+    set -U THEME "tokyonight-dark"
     set tmux_path (nix-build -E 'let pkgs = import <nixpkgs> {}; tokyonight = pkgs.tmuxPlugins.mkTmuxPlugin { pluginName = "tokyonight"; version = "latest"; src = pkgs.fetchFromGitHub { owner = "fabioluciano"; repo = "tmux-tokyo-night"; rev = "main"; sha256 = "sha256-9nDgiJptXIP+Hn9UY+QFMgoghw4HfTJ5TZq0f9KVOFg="; }; }; in tokyonight')
-    $tmux_path/share/tmux-plugins/tokyonight/tmux-tokyo-night.tmux
+    $tmux_path/share/tmux-plugins/tokyonight/tmux-tokyo-night.tmux 2&> /dev/null
     for server_name in /run/user/1000/nvim.* # maybe should be updated later lol
         nvim --server "$server_name" --remote-send ":lua vim.cmd('colorscheme tokyonight-night')<CR>"
     end
+    hyprctl keyword general:col.active_border "rgba(a9b1d6ff)" 2&> /dev/null
+    hyprctl keyword general:col.inactive_border "rgba(1a1b26ff)" 2&> /dev/null
 else if test "$argv[1]" = "--gruvbox-light"
-    set -Ux THEME "gruvbox-light"
+    echo "gruvbox-light choice"
+    set -U THEME "gruvbox-light"
     set tmux_path $(nix eval --raw nixpkgs#tmuxPlugins.gruvbox.outPath)
-    $tmux_path/share/tmux-plugins/gruvbox/gruvbox-tpm.tmux
+    $tmux_path/share/tmux-plugins/gruvbox/gruvbox-tpm.tmux 2&> /dev/null
     for server_name in /run/user/1000/nvim.* # maybe should be updated later lol
         nvim --server "$server_name" --remote-send ":lua vim.o.background = 'light'<CR>"
         nvim --server "$server_name" --remote-send ":lua vim.cmd('colorscheme gruvbox')<CR>"
     end
+    hyprctl keyword general:col.active_border "rgba(3c3836ff)" 2&> /dev/null
+    hyprctl keyword general:col.inactive_border "rgba(fbf1c7ff)" 2&> /dev/null
 end
+echo "THEME: $THEME"
 kitty_reload
 hyprland_reload
 wallpaper_reload
