5fa8deda0bd4c2a5172473ed1cc5f54a975cc4be
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-21 12:05:47 +0100

    mako is good enough

--- home/optional/pkgs/mako/default.nix
+++ home/optional/pkgs/mako/default.nix
@@ -3,34 +3,9 @@ let
   # homeDir = builtins.getEnv "HOME";
   homeDir = config.home.homeDirectory;
 in {
-    # https://www.mankier.com/5/mako
-    services.mako = {
-        enable = true;
-        # null or one of "top-right", "top-center", "top-left", "bottom-right", "bottom-center", "bottom-left", "center"
-        anchor = "bottom-right";
-        borderRadius = 4;
-        borderSize = 2;
-        font = "FiraCode Nerd Font Mono 10";
-        format = "<b>%s</b>\\n%b";
-        groupBy = null;
-        height = 100;
-        width = 300;
-        margin = "5, 0";
-        padding = "5";
-        iconPath = "${homeDir}/.icons";
-        icons = true;
-        ignoreTimeout = false;
-        # null or one of "background", "bottom", "top", "overlay"
-        layer = "top";
-        # If 1, enable Pango markup. If 0, disable Pango markup. If enabled, Pango markup will be interpreted in your format specifier and in the body of notifications.
-        markup = true;
-        maxIconSize = 32;
-        maxVisible = 5;
-        output = null;
-        # null or one of "+time", "-time", "+priority", "-priority"
-        sort = "-time";
-        defaultTimeout = 5000;
-        extraConfig = builtins.toString ./config;
+  # https://www.mankier.com/5/mako
+  services.mako = {
+    enable = true;
   };
 
   xdg.configFile."mako/theme".source = ./theme;
--- home/scripts/misc/theme_reloader.fish
+++ home/scripts/misc/theme_reloader.fish
@@ -6,8 +6,8 @@ function kitty_reload
 end
 
 function mako_reload 
-    ln -sf ~/.config/mako/theme/$THEME.conf ~/.config/mako/theme.conf
-    kill -SIGUSR1 $(pgrep kitty)
+    pkill mako
+    mako -c ~/.config/mako/theme/$THEME.conf &
 end
 
 function hyprland_reload 
@@ -25,7 +25,6 @@ if test "$argv[1]" = "--tokyonight-dark"
     hyprland_reload
     hyprctl keyword general:col.active_border "rgba(a9b1d6ff)" 2&> /dev/null
     hyprctl keyword general:col.inactive_border "rgba(1a1b26ff)" 2&> /dev/null
-
 else if test "$argv[1]" = "--gruvbox-light"
     set -U THEME "gruvbox-light"
     set tmux_path $(nix eval --raw nixpkgs#tmuxPlugins.gruvbox.outPath)
